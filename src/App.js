9c04fc582e69441ca20564df12ec6c294c9e9882
changlc125 <changlc125@gmail.com>
2022-04-07 00:08:19 -0400

    css style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App bar </nav>
+      <nav className="navbar board">Board bar </nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" />
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" />
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
